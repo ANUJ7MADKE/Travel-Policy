a4c79628412b5f58627d935c8edc748f0ee520c2
AnamayNarkar <anamay.narkar.102@gmail.com>
2024-10-31 22:28:24 +0530

    segregated different parts of application form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import {Login,Dashboard,ApplicationForm,About,Policy, Applications} from './pages'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Login, Dashboard, ApplicationForm, About, Policy, Applications } from './pages'
 import './App.css'
 import LoginRoot from './components/LoginRoot/LoginRoot'
 import userDataLoader from './services/userDataLoader';
@@ -8,31 +8,34 @@ import { applicationStatusAction } from './services/applicationStatusAction';
 import Root from './components/DashboardRoot/Root';
 
 const router = createBrowserRouter([
-  { path: "/", element: <LoginRoot />, children: [
-      { index: true, element: <Login /> },
-      { path: "about", element: <About /> },
-      { path: "policy", element: <Policy /> }
-    ]
-  },
-  { path: "/applicant", element: <Root/>, id: "Applicant-Root", loader: userDataLoader, 
-    children: [
-      { path: "dashboard", element: <Dashboard/> },
-      { path: "dashboard/:status", element: <Applications/> },
-      { path: "form", element: <ApplicationForm />, action: createApplicationAction },
-      { path: "faqs", element: <h1>FAQs</h1> },
-      { path: "contact-us", element: <h1>Contact Us</h1> },
-    ]
-  },
-  { path: "/validator", element: <Root/>, id: "Validator-Root", loader: userDataLoader,
-    children: [
-      { path: "dashboard", element: <Dashboard/> },
-      { path: "dashboard/:status", element: <Applications />, action: applicationStatusAction },
-    ]
-  },
+        {
+                path: "/", element: <LoginRoot />, children: [
+                        { index: true, element: <Login /> },
+                        { path: "about", element: <About /> },
+                        { path: "policy", element: <Policy /> }
+                ]
+        },
+        {
+                path: "/applicant", element: <Root />, id: "Applicant-Root", loader: userDataLoader,
+                children: [
+                        { path: "dashboard", element: <Dashboard /> },
+                        { path: "dashboard/:status", element: <Applications /> },
+                        { path: "form", element: <ApplicationForm />, action: createApplicationAction },
+                        { path: "faqs", element: <h1>FAQs</h1> },
+                        { path: "contact-us", element: <h1>Contact Us</h1> },
+                ]
+        },
+        {
+                path: "/validator", element: <Root />, id: "Validator-Root", loader: userDataLoader,
+                children: [
+                        { path: "dashboard", element: <Dashboard /> },
+                        { path: "dashboard/:status", element: <Applications />, action: applicationStatusAction },
+                ]
+        },
 ]);
 
 function App() {
-  return <RouterProvider router={router}/>
+        return <RouterProvider router={router} />
 }
 
 export default App
